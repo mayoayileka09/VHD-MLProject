16f85137ca37734ee10f9185f17ebd29398c3865
mayoayielka <150277360+mayoayielka@users.noreply.github.com>
2026-01-13 19:47:43 -0600

    Finished Baseline Model Definitions

--- src/models.py
+++ src/models.py
@@ -4,16 +4,29 @@ from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor,HistGr
 def get_vhd_models(random_state: int = 42):
     return {
         "logreg": LogisticRegression(
-            max_iter=2000,
-            class_weight="balanced",  # helps if VHD is imbalanced
-            n_jobs=None
+            max_iter=2000, # Controls how many optimization steps are allowed
+            class_weight="balanced",  # helps if VHD is imbalanced // automatically adjusts weights based on class freq.
+            n_jobs=None 
         ),
         "rf": RandomForestClassifier(
-            n_estimators=400,
+            n_estimators=400, # number of trees
             random_state=random_state,
-            n_jobs=-1
+            n_jobs=-1 # uses all available cpu resources
         ),
         "hgb": HistGradientBoostingClassifier(
+            random_state=random_state
+        )
+    }
+
+def get_ef_models(random_state: int = 42):
+    return{
+        "ridge": Ridge(random_state = random_state),
+        "rf": RandomForestRegressor(
+            n_estimators= 400,
+            random_state=random_state,
+            n_jobs = -1
+        ),
+        "hgb": HistGradientBoostingRegressor(
             random_state=random_state
         )
     }
